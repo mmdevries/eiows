4f65d8de62f4d51ff883a0f36a78f2ec29679970
Matthijs de Vries <matthijsdevries@bcnexxt.com>
2019-12-11 07:40:37 +0000

    Revert Epoll update

--- uWebSockets/src/Epoll.h
+++ uWebSockets/src/Epoll.h
@@ -34,7 +34,6 @@ struct Loop {
     int epfd;
     int numPolls = 0;
     bool cancelledLastTimer;
-    Timer *processingTimer = nullptr; // the timer we're currently processing a callback for
     int delay = -1;  // delay to next timer expiry, or -1 if no timers pending
     epoll_event readyEvents[1024];
     std::chrono::system_clock::time_point timepoint;
@@ -114,10 +113,7 @@ struct Timer {
             }
             pos++;
         }
-
-        if(loop->processingTimer == this) {
-            loop->cancelledLastTimer = true;
-        }
+        loop->cancelledLastTimer = true;
 
         loop->delay = -1;
         if (loop->timers.size()) {
