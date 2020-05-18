8488ef60e7f3fc9ca3231afee5e5c5048490ad38
Matthijs de Vries <matthijsdevries@bcnexxt.com>
2020-05-18 19:14:50 +0000

    Remove unused Epoll

--- uWebSockets/src/Backend.h
+++ uWebSockets/src/Backend.h
@@ -1,15 +1,9 @@
 #ifndef BACKEND_H
 #define BACKEND_H
 
-// Default to Epoll if nothing specified and on Linux
 // Default to Libuv if nothing specified and not on Linux
 #if !defined(__linux__) || defined(USE_LIBUV)
 #include "Libuv.h"
-#else
-#ifndef USE_EPOLL
-#define USE_EPOLL
-#endif
-#include "Epoll.h"
 #endif
 
 #endif // BACKEND_H
